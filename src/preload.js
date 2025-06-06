321f699f6faa990db4660e448c57a870f9ce3ef1
SigurdFPS <rigginscody@gmail.com>
2025-06-06 13:36:11 -0500

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,8 +1,17 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
+  // Auth
   authorizeYouTube: () => ipcRenderer.invoke('authorize-youtube'),
+
+  // Analysis
   analyzeComments: (videoLink) => ipcRenderer.invoke('analyze-comments', videoLink),
+
+  // Deletion
   deleteHighlyLikely: () => ipcRenderer.invoke('delete-highly-likely'),
   deleteReviewedComments: () => ipcRenderer.invoke('delete-reviewed-comments'),
+
+  // Manual review modal support
+  getReviewComments: () => ipcRenderer.invoke('get-review-comments'),
+  submitReviewedComments: (commentIds) => ipcRenderer.send('submit-reviewed-comments', commentIds),
 });
