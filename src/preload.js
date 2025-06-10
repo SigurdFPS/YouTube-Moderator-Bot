6759225d7421a4cc948cf205a490bc6b982ad57e
SigurdFPS <rigginscody@gmail.com>
2025-06-10 12:28:14 -0500

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -20,17 +20,18 @@ contextBridge.exposeInMainWorld('api', {
   startLiveMonitor: (videoId) => ipcRenderer.send('start-live-monitor', videoId),
   stopLiveMonitor: () => ipcRenderer.send('stop-live-monitor'),
 
-  // Live chat message feed from main process
   onLiveChatMessage: (callback) => {
     ipcRenderer.removeAllListeners('live-chat-message');
     ipcRenderer.on('live-chat-message', (_, msg) => callback(msg));
   },
 
-  // Listen once for monitor stop
   onLiveMonitorStopped: (callback) => {
     ipcRenderer.once('live-monitor-stopped', () => callback());
   },
 
-  // Spam deletion during live (if needed in renderer directly)
   deleteLiveComment: (commentId) => ipcRenderer.invoke('delete-live-comment', commentId),
+
+  // === Config Persistence ===
+  loadConfig: () => ipcRenderer.invoke('load-config'),
+  saveConfig: (config) => ipcRenderer.send('save-config', config),
 });
